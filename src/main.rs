939b9cd707cc61729592d1b3db64d66b30269141
liebman <liebman@zod.com>
2025-08-09 10:35:40 -0700

    simplify some - wifi init like esp-hal examples - http request via tcp socket - remove unneeded cargo entries

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![no_std]
 #![no_main]
-#![feature(impl_trait_in_assoc_type)]
+
+use core::net::Ipv4Addr;
 
 use embassy_executor::Spawner;
 use embassy_net::Config;
-use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
-use embassy_sync::signal::Signal;
 use embassy_time::Duration;
+use embassy_time::Timer;
+use embassy_net::tcp::TcpSocket;
 use embedded_graphics::Drawable;
 use embedded_graphics::geometry::Point;
 use embedded_graphics::mono_font::MonoTextStyleBuilder;
@@ -17,7 +18,6 @@ use embedded_graphics::text::Text;
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::gpio::Pin;
-// use esp_hal::peripherals;
 use esp_hal::rng::Rng;
 use esp_hal::time::Rate;
 use esp_hal::timer::systimer::SystemTimer;
@@ -29,12 +29,14 @@ use esp_hub75::framebuffer::compute_frame_count;
 use esp_hub75::framebuffer::compute_rows;
 use esp_hub75::framebuffer::plain::DmaFrameBuffer;
 use esp_println::logger::init_logger_from_env;
+use esp_wifi::wifi::ClientConfiguration;
+use esp_wifi::wifi::Configuration;
+use esp_wifi::wifi::WifiController;
+use esp_wifi::wifi::WifiDevice;
+use esp_wifi::wifi::WifiEvent;
+use esp_wifi::wifi::WifiState;
 use log::*;
-// use alloc::string::ToString;
-// use alloc::string::ToString;
 extern crate alloc;
-use alloc::string::ToString;
-use log::info;
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -45,8 +47,6 @@ const NROWS: usize = compute_rows(ROWS);
 const FRAME_COUNT: usize = compute_frame_count(BITS);
 
 type FBType = DmaFrameBuffer<ROWS, COLS, NROWS, BITS, FRAME_COUNT>;
-type FrameBufferExchange = Signal<CriticalSectionRawMutex, &'static mut FBType>;
-
 pub struct Hub75Peripherals<'d> {
     pub lcd_cam: esp_hal::peripherals::LCD_CAM<'d>,
     pub dma_channel: esp_hal::peripherals::DMA_CH0<'d>,
@@ -62,6 +62,39 @@ macro_rules! mk_static {
     }};
 }
 
+const WIFI_SSID: &str = env!("WIFI_SSID");
+const WIFI_PWD: &str = env!("WIFI_PWD");
+
+
+#[embassy_executor::task]
+async fn hub75_task(peripherals: Hub75Peripherals<'static>, fb: &'static mut FBType) {
+    info!("hub75_task: starting!");
+
+    // let (_, tx_descriptors) = esp_hal::dma_descriptors!(0, FBType::dma_buffer_size_bytes());
+    let (_, tx_descriptors) = esp_hal::dma_descriptors!(0, FBType::dma_buffer_size_bytes());
+    let mut hub75 = Hub75::new_async(
+        peripherals.lcd_cam,
+        peripherals.pins,
+        peripherals.dma_channel,
+        tx_descriptors,
+        Rate::from_mhz(20),
+    )
+    .expect("failed to create Hub75!");
+
+    loop {
+        let mut xfer = hub75
+            .render(fb)
+            .map_err(|(e, _hub75)| e)
+            .expect("failed to start render!");
+        xfer.wait_for_done()
+            .await
+            .expect("rendering wait_for_done failed!");
+        let (result, new_hub75) = xfer.wait();
+        hub75 = new_hub75;
+        result.expect("transfer failed");
+    }
+}
+
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
     init_logger_from_env();
@@ -69,68 +102,38 @@ async fn main(spawner: Spawner) {
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_alloc::heap_allocator!(size: 72 * 1024);
 
-    let wifi_ssid = option_env!("WIFI_SSID")
-        .unwrap_or("default_ssid")
-        .to_string();
-    let wifi_pwd = option_env!("WIFI_PWD")
-        .unwrap_or("default_password")
-        .to_string();
-    info!("WIFI_SSID: {wifi_ssid}, WIFI_PWD: {wifi_pwd}");
     // Init Embassy
     let system_timer = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(system_timer.alarm0);
+
+    // Init WiFi
     let mut rng = Rng::new(peripherals.RNG);
-    let init = &*mk_static!(
+    let init = mk_static!(
         esp_wifi::EspWifiController<'static>,
         esp_wifi::init(timg0.timer0, rng).unwrap()
     );
     let wifi = peripherals.WIFI;
 
-    let (mut controller, interfaces) = esp_wifi::wifi::new(init, wifi).unwrap();
+    let (controller, interfaces) = esp_wifi::wifi::new(init, wifi).unwrap();
 
     // let (wifi_sta_interface, controller) = esp_wifi::wifi::new(init,
     // wifi).unwrap();
 
     let sta_config = Config::dhcpv4(Default::default());
 
-    let seed = rng.random();
+    let seed = (rng.random() as u64) << 32 | rng.random() as u64;
 
-    let (sta_stack, sta_runner) = embassy_net::new(
+    let (stack, runner) = embassy_net::new(
         interfaces.sta,
         sta_config,
         mk_static!(
             embassy_net::StackResources<3>,
             embassy_net::StackResources::<3>::new()
         ),
-        seed.into(),
+        seed,
     );
-
-    spawner.must_spawn(net_task(sta_runner));
-
-    let config = esp_wifi::wifi::Configuration::Client(esp_wifi::wifi::ClientConfiguration {
-        ssid: wifi_ssid,
-        password: wifi_pwd,
-        ..Default::default()
-    });
-
-    controller.set_configuration(&config).unwrap();
-    controller.start().unwrap();
-    controller.connect().unwrap();
-
-    loop {
-        debug!(
-            "waiting for wifi: link_up={:?}, config_up={}, ip={:?}",
-            sta_stack.is_link_up(),
-            sta_stack.is_config_up(),
-            sta_stack.config_v4().clone().map(|ip| ip.address),
-        );
-
-        if sta_stack.is_link_up() && sta_stack.is_config_up() {
-            info!("WiFi connected!");
-            break;
-        };
-        embassy_time::Timer::after(embassy_time::Duration::from_millis(1000)).await;
-    }
+    spawner.spawn(connection(controller)).ok();
+    spawner.spawn(net_task(runner)).ok();
 
     // // portal3 pins
     // let pins = Hub75Pins16 {
@@ -176,11 +179,23 @@ async fn main(spawner: Spawner) {
         pins,
     };
 
+    info!("Rendering display");
     let fb0 = mk_static!(FBType, FBType::new());
-    let fb1 = mk_static!(FBType, FBType::new());
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_5X7)
+        .text_color(Color::YELLOW)
+        .background_color(Color::BLACK)
+        .build();
 
-    static TX: FrameBufferExchange = FrameBufferExchange::new();
-    static RX: FrameBufferExchange = FrameBufferExchange::new();
+    fb0.erase();
+    Text::with_alignment(
+        "Hello, World!",
+        Point::new(32, ROWS as i32 / 2 - 1),
+        text_style,
+        Alignment::Center,
+    )
+    .draw(fb0)
+    .expect("failed to draw text");
 
     let cpu1_fnctn = {
         move || {
@@ -189,7 +204,7 @@ async fn main(spawner: Spawner) {
             let lp_executor = mk_static!(Executor, Executor::new());
             // display task runs as low priority task
             lp_executor.run(|spawner| {
-                spawner.spawn(hub75_task(hub75_per, &RX, &TX, fb1)).ok();
+                spawner.spawn(hub75_task(hub75_per, fb0)).ok();
             });
         }
     };
@@ -206,214 +221,111 @@ async fn main(spawner: Spawner) {
         .start_app_core(app_core_stack, cpu1_fnctn)
         .unwrap();
 
-    spawner.must_spawn(wifi_spam_task(sta_stack, rng));
-    spawner.must_spawn(display_task(&TX, &RX, fb0));
+    let mut rx_buffer = [0; 4096];
+    let mut tx_buffer = [0; 4096];
 
     loop {
-        embassy_time::Timer::after(Duration::from_millis(100)).await;
+        if stack.is_link_up() {
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+
+    info!("Waiting to get IP address...");
+    loop {
+        if let Some(config) = stack.config_v4() {
+            info!("Got IP: {}", config.address);
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+
+    loop {
+        Timer::after(Duration::from_millis(500)).await;
+
+        let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+
+        socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
+
+        let remote_endpoint = (Ipv4Addr::new(142, 250, 185, 115), 80);
+        info!("connecting...");
+        let r = socket.connect(remote_endpoint).await;
+        if let Err(e) = r {
+            info!("connect error: {e:?}");
+            continue;
+        }
+        info!("connected!");
+        let mut buf = [0; 1024];
+        loop {
+            use embedded_io_async::Write;
+            let r = socket
+                .write_all(b"GET / HTTP/1.0\r\nHost: www.mobile-j.de\r\n\r\n")
+                .await;
+            if let Err(e) = r {
+                warn!("write error: {e:?}");
+                break;
+            }
+            let n = match socket.read(&mut buf).await {
+                Ok(0) => {
+                    info!("read EOF");
+                    break;
+                }
+                Ok(n) => n,
+                Err(e) => {
+                    info!("read error: {e:?}");
+                    break;
+                }
+            };
+            info!("{}", core::str::from_utf8(&buf[..n]).unwrap_or("read error"));
+        }
+        Timer::after(Duration::from_millis(500)).await;
     }
 }
 
 #[embassy_executor::task]
-pub async fn wifi_spam_task(stack: embassy_net::Stack<'static>, rng: Rng) {
-    // let rx_buf = mk_static!([u8; 512], [0u8; 512]);
-    // let tx_buf = mk_static!([u8; 512], [0u8; 512]);
-    // let rx_meta = mk_static!([PacketMetadata<UdpMetadata>; 4], [PacketMetadata::EMPTY; 4]);
-    // let tx_meta = mk_static!([PacketMetadata<UdpMetadata>; 4], [PacketMetadata::EMPTY; 4]);
-    // info!("Starting UDP socket task");
-    // let mut socket = UdpSocket::new(stack, rx_meta, rx_buf, tx_meta, tx_buf);
-    // socket.bind(12345).unwrap();
-
-    // let packet = [0x42; 64];
-    // let dest = (Ipv4Address::new(8, 8, 8, 8), 12345);
-
-    let mut client = HTTPClient::new(
-        HTTPConfig {
-            base_url: "http://api.wmata.com".into(),
-        },
-        stack,
-        rng,
-    );
+async fn connection(mut controller: WifiController<'static>) {
+    info!("start connection task");
+    info!("Device capabilities: {:?}", controller.capabilities());
     loop {
-        info!("SPAM");
-        client
-            .request("/Rail.svc/json/jStations", &mut [0u8; 32 * 1024])
-            .await;
-        embassy_time::Timer::after(Duration::from_millis(2000)).await;
+        match esp_wifi::wifi::wifi_state() {
+            WifiState::StaConnected => {
+                // wait until we're no longer connected
+                controller.wait_for_event(WifiEvent::StaDisconnected).await;
+                Timer::after(Duration::from_millis(5000)).await
+            }
+            _ => {}
+        }
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_config = Configuration::Client(ClientConfiguration {
+                ssid: WIFI_SSID.into(),
+                password: WIFI_PWD.into(),
+                ..Default::default()
+            });
+            controller.set_configuration(&client_config).unwrap();
+            info!("Starting wifi");
+            controller.start_async().await.unwrap();
+            info!("Wifi started!");
+
+            info!("Scan");
+            let result = controller.scan_n_async(10).await.unwrap();
+            for ap in result {
+                info!("{ap:?}");
+            }
+        }
+        info!("About to connect...");
+
+        match controller.connect_async().await {
+            Ok(_) => info!("Wifi connected!"),
+            Err(e) => {
+                info!("Failed to connect to wifi: {e:?}");
+                Timer::after(Duration::from_millis(5000)).await
+            }
+        }
     }
 }
 
-#[embassy_executor::task(pool_size = 3)]
-pub async fn net_task(
-    mut runner: embassy_net::Runner<'static, esp_wifi::wifi::WifiDevice<'static>>,
-) {
+#[embassy_executor::task]
+async fn net_task(mut runner: embassy_net::Runner<'static, WifiDevice<'static>>) {
     runner.run().await
 }
 
-#[embassy_executor::task]
-async fn hub75_task(
-    peripherals: Hub75Peripherals<'static>,
-    rx: &'static FrameBufferExchange,
-    tx: &'static FrameBufferExchange,
-    fb: &'static mut FBType,
-) {
-    info!("hub75_task: starting!");
-
-    // let (_, tx_descriptors) = esp_hal::dma_descriptors!(0, FBType::dma_buffer_size_bytes());
-    let (_, tx_descriptors) = esp_hal::dma_descriptors!(0, FBType::dma_buffer_size_bytes());
-    let mut hub75 = Hub75::new_async(
-        peripherals.lcd_cam,
-        peripherals.pins,
-        peripherals.dma_channel,
-        tx_descriptors,
-        Rate::from_mhz(20),
-    )
-    .expect("failed to create Hub75!");
-
-    let mut fb = fb;
-
-    loop {
-        if rx.signaled() {
-            let new_fb = rx.wait().await;
-            tx.signal(fb);
-            fb = new_fb;
-        }
-
-        let mut xfer = hub75
-            .render(fb)
-            .map_err(|(e, _hub75)| e)
-            .expect("failed to start render!");
-        xfer.wait_for_done()
-            .await
-            .expect("rendering wait_for_done failed!");
-        let (result, new_hub75) = xfer.wait();
-        hub75 = new_hub75;
-        result.expect("transfer failed");
-
-        // embassy_time::Timer::after(Duration::from_millis(250)).await;
-    }
-}
-
-#[embassy_executor::task]
-async fn display_task(
-    rx: &'static FrameBufferExchange,
-    tx: &'static FrameBufferExchange,
-    mut fb: &'static mut FBType,
-) {
-    info!("Starting display");
-    let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_5X7)
-        .text_color(Color::YELLOW)
-        .background_color(Color::BLACK)
-        .build();
-    let point = Point::new(32, ROWS as i32 / 2 - 1);
-
-    loop {
-        fb.erase();
-        Text::with_alignment("Hello, World!", point, text_style, Alignment::Center)
-            .draw(fb)
-            .expect("failed to draw text");
-        tx.signal(fb);
-        fb = rx.wait().await;
-    }
-}
-
-//use anyhow::Result;
-use alloc::format;
-use alloc::string::String;
-use embassy_net::{
-    Stack,
-    dns::DnsSocket,
-    tcp::client::{TcpClient, TcpClientState},
-};
-use reqwless::request::RequestBuilder;
-use reqwless::{
-    client::{HttpClient, TlsConfig, TlsVerify},
-    request::Method,
-};
-
-pub struct HTTPConfig {
-    // pub api_key: String,
-    pub base_url: String,
-}
-
-pub struct HTTPClient {
-    config: HTTPConfig,
-
-    /// Wifi stack
-    stack: Stack<'static>,
-
-    /// Random numbers generator
-    rng: esp_hal::rng::Rng,
-
-    /// TCP client state
-    tcp_client_state: TcpClientState<1, 1024, 1024>,
-
-    /// Buffer for received TLS data
-    read_record_buffer: [u8; 1024],
-
-    /// Buffer for transmitted TLS data
-    write_record_buffer: [u8; 1024], /* tcp_client: TcpClient<'static, 1,                                       * 1024, 1024>,
-                                          * dns_socket: DnsSocket<'static> */
-}
-
-impl HTTPClient {
-    pub fn new(config: HTTPConfig, stack: Stack<'static>, rng: esp_hal::rng::Rng) -> Self {
-        let tcp_client_state = TcpClientState::<1, 1024, 1024>::new();
-        // let tcp_client = TcpClient::new(stack, &tcp_client_state);
-        // let dns_socket = DnsSocket::new(stack);
-        Self {
-            config,
-            stack,
-            rng,
-            tcp_client_state,
-            read_record_buffer: [0_u8; 1024],
-            write_record_buffer: [0_u8; 1024], /* tcp_client,
-                                                    * dns_socket */
-        }
-    }
-
-    async fn request(&mut self, path: &str, body: &mut [u8]) -> () {
-        let url = format!(
-            "{}/{}",
-            self.config.base_url.trim_end_matches('/'),
-            path.trim_start_matches('/')
-        );
-
-        let headers: [(&str, &str); 2] = [
-            ("api_key", "97c18244899442f1b781a0a975807f16"),
-            ("Content-Type", "application/json"),
-        ];
-
-        // let dns_socket = DnsSocket::new(self.stack);
-
-        let seed = self.rng.random() as u64;
-        let tls_config = TlsConfig::new(
-            seed,
-            &mut self.read_record_buffer,
-            &mut self.write_record_buffer,
-            TlsVerify::None,
-        );
-
-        let dns_socket = DnsSocket::new(self.stack);
-        let tcp_client = TcpClient::new(self.stack, &self.tcp_client_state);
-
-        let mut client = HttpClient::new_with_tls(&tcp_client, &dns_socket, tls_config);
-
-        let mut request = client
-            .request(Method::GET, &url)
-            .await
-            .unwrap()
-            .headers(&headers);
-
-        let mut header_buf = [0u8; 2048];
-        let response = request.send(&mut header_buf).await.unwrap();
-        let response_body = response.body();
-        let mut body_reader = response_body.reader();
-        let num_bytes = body_reader.read_to_end(body).await.unwrap();
-        info!("bytes {num_bytes:?}");
-        // let body_str = core::str::from_utf8(body).unwrap();
-        // info!("BODY: {}", body_str);
-        //  Ok(serde_json::from_slice::<U>(&body[..num_bytes])?)
-    }
-}
